35b159034971065d25605d48b74647f1e95b2555
MacoRios <riosmaco518@gmail.com>
2022-05-18 23:00:23 -0600

    Casi matando

--- resources/js/src/views/Microprestamos/Abono_prestamo/abonoPrestamo.vue
+++ resources/js/src/views/Microprestamos/Abono_prestamo/abonoPrestamo.vue
@@ -24,7 +24,7 @@
             <span>Cantidad de abono(Q)</span>
           </div>
           <div class="vx-col sm:w-2/3 w-full">
-            <vs-input type="number" class="w-full" v-model="cantidad_abono"  name="cantidad" v-validate="'required|max:5|numeric'"/>
+            <vs-input type="number" class="w-full" v-model="cantidad_abono"  name="cantidad" v-validate="'required|max:5'"/>
             <span class="text-danger text-sm" v-show="errors.has('cantidad')">{{ errors.first('cantidad') }}</span>
             <span v-if ="dias > 0 && mes === 0" >{{ 'Dias atrasados:'+ ' ' + dias+ ' '}}</span>
             <span v-if="mes > 0" >{{'Meses atrasados:'+' '+ mes}} </span>
@@ -135,7 +135,7 @@ const dict = {
     cantidad: {
       	  required: 'El campo cantidad de abono es requerido',
       	  numeric: 'El campo solo debe de contener números',
-      max: 'Este campo solo acepta hasta 5 dígitos'
+        max: 'Este campo solo acepta hasta 5 dígitos'
     },
     descripcion: {
       required:'El campo descripción es requerido',
